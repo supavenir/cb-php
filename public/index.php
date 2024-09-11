1bb1afb330ad07afd8cd812760b6320c3e926ec2
J-C HERON <myaddressmail@gmail.com>
2024-09-11 11:09:41 +0200

    add daisy

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Pecee\SimpleRouter\SimpleRouter;
 require "./../vendor/autoload.php";
 
 $loader = new \Twig\Loader\FilesystemLoader('./../src/templates');
-$twig = new \Twig\Environment($loader, []);
+$twig = new \Twig\Environment($loader, ["file_name_pattern"=>"*.html.twig"]);
 
 require "./../src/config/router.php";
 
