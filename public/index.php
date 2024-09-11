1c469069b4e730014a727d6799e76cfd9189c7e6
J-C HERON <myaddressmail@gmail.com>
2024-09-11 12:38:28 +0200

    create compte

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 require "./../vendor/autoload.php";
 
+session_start();
+
 $loader = new \Twig\Loader\FilesystemLoader('./../src/templates');
 $twig = new \Twig\Environment($loader, ["file_name_pattern"=>"*.html.twig"]);
 
--- src/config/router.php
+++ src/config/router.php
@@ -3,20 +3,21 @@ use Pecee\SimpleRouter\SimpleRouter;
 use App\models\CompteBancaire;
 
 
-
 SimpleRouter::get('/', function() {
-    return 'Hello world';
-});
-
-SimpleRouter::get('/cb/{solde?}', function($solde=0) {
     global $twig;
-    $cb=new CompteBancaire("Toto",$solde);
+    $cb=$_SESSION['cb']??null;
     return $twig->render('cbView.html.twig',['cb'=>$cb]);
 });
 
-SimpleRouter::get('/op/{solde?}/{montant?}', function($solde=0, $montant=0){
+SimpleRouter::get('/newCompte', function() {
     global $twig;
-    $cb=new CompteBancaire("Toto",$solde);
-    $cb->deposer("depot", $montant);
+    return $twig->render('newCompte.html.twig');
+});
+
+SimpleRouter::post('/create', function(){
+    global $twig;
+    $titulaire=$_POST['titulaire'];
+    $cb=new CompteBancaire($titulaire);
+    $_SESSION['cb']=$cb;
     return $twig->render('cbView.html.twig',['cb'=>$cb]);
 });
