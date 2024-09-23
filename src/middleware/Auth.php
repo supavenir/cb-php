3fab6708c856512f306b6ac84663d9217322b1de
J-C HERON <myaddressmail@gmail.com>
2024-09-23 17:09:17 +0200

    add Auth methods

--- src/middleware/Auth.php
+++ src/middleware/Auth.php
@@ -2,18 +2,34 @@
 
 namespace App\middleware;
 
+use App\models\Utilisateur;
 use Pecee\Http\Middleware\IMiddleware;
 use Pecee\Http\Request;
-use Pecee\SimpleRouter\Router;
 use Pecee\SimpleRouter\SimpleRouter;
 
 class Auth implements IMiddleware
 {
     const USER_KEY="activeUser";
 
+    public function connect(Utilisateur $user):void{
+        $_SESSION[self::USER_KEY]=$user;
+    }
+
+    public function getActiveUser():?Utilisateur{
+        return $_SESSION[self::USER_KEY]??null;
+    }
+
+    public function disconnect(){
+        if(isset($_SESSION[self::USER_KEY])) {
+            $_SESSION[self::USER_KEY] = null;
+            unset($_SESSION[self::USER_KEY]);
+        }
+        session_destroy();
+    }
+
     public function handle(Request $request): void
     {
-     $user=$_SESSION[self::USER_KEY]??null;
+     $user=$this->getActiveUser();
      if($user==null){
          SimpleRouter::response()->redirect('/login');
      }
